8744020e01050f4f0987bb3bfa38cb1a5fdb940d
AndraTodor <andra_t27@yahoo.com>
2024-09-10 15:47:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import 'index.css'; // Asigură-te că tailwind.css este inclus în index.css
+import 'index.css';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    // Încărcăm contactele din localStorage la inițializarea aplicației
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    // Salvăm contactele în localStorage ori de câte ori acestea se modifică
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (name, number) => {
     // Verifică dacă contactul există deja
